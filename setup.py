b63ac223aa921660b25435cf40323a9fea65424e
Runzhe Wang <15501019889@126.com>
2021-10-26 11:19:55 +0800

    style: modify setup scripts, remove requirements of pbr
    Signed-off-by: Runzhe Wang <15501019889@126.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(setup_requires=['pbr'], pbr=True)
+long_description = ""
+
+setup(
+    name = "keentune-bench",
+    version = "1.0.0",
+    description = "KeenTune bench unit",
+    long_description = long_description,
+    url = "https://codeup.openanolis.cn/codeup/keentune/keentune_bench",
+    license = "Apache License",
+    classifiers = [
+        "Environment:: KeenTune",
+        "IntendedAudience :: Information Technology",
+        "IntendedAudience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "ProgrammingLanguage :: Python"
+    ],
+
+    packages = find_packages(),
+    package_data={'bench': ['bench.conf']},
+    
+    data_files = [
+        ("/etc/keentune/conf", ["bench/bench.conf"]),
+    ],
+)
