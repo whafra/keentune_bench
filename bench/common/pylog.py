5d2ae8a9cb4959f47a9146d362c73da537ff8d2d
runzhe.wrz <runzhe.wrz@alibaba-inc.com>
2022-07-01 15:37:32 +0800

    feat: run benchmark in multiple times
    1. add 'round' args to define the running times
    2. simplify return data.
    	e.g. {'metrics': {'value': 10}} -> {'metrics':10}
    
    Signed-off-by: runzhe.wrz <runzhe.wrz@alibaba-inc.com>

--- bench/common/pylog.py
+++ bench/common/pylog.py
@@ -68,26 +68,18 @@ def functionLog(func):
         global CALL_LEVEL, PLACEHOLDER
         CALL_LEVEL += 1
 
-        if CALL_LEVEL == 0:
-            logger.info("{placeholder}[{module}.{func}] << {args} {kw}".format(
-                        placeholder=PLACEHOLDER*CALL_LEVEL,
-                        module=func.__module__,
-                        func=func.__qualname__,
-                        args=",".join(["{}".format(_arg) for _arg in args]),
-                        kw=",".join(["{} = {}".format(k, v) for k, v in kw.items()])))
-        else:
-            logger.debug("{placeholder}[{module}.{func}] << {args} {kw}".format(
-                placeholder=PLACEHOLDER*CALL_LEVEL,
-                module=func.__module__,
-                func=func.__qualname__,
-                args=",".join(["{}".format(_arg) for _arg in args]),
-                kw=",".join(["{} = {}".format(k, v) for k, v in kw.items()])))
+        logger.debug("{placeholder}[{module}.{func}] << {args} {kw}".format(
+                    placeholder=PLACEHOLDER*CALL_LEVEL,
+                    module=func.__module__,
+                    func=func.__qualname__,
+                    args=",".join(["{}".format(_arg) for _arg in args]),
+                    kw=",".join(["{} = {}".format(k, v) for k, v in kw.items()])))
 
         try:
-            suc, res = func(*args, **kw)
+            out = func(*args, **kw)
 
         except Exception as e:
-            logger.critical('[{module}.{func}] {trace}'.format(
+            logger.error('[{module}.{func}] {trace}'.format(
                 module=func.__module__,
                 func=func.__qualname__,
                 trace=traceback.format_exc()))
@@ -95,116 +87,13 @@ def functionLog(func):
             return False, e
 
         else:
-            if suc:
-                if CALL_LEVEL == 0:
-                    logger.info("{placeholder}[{module}.{func}] >> {out}".format(
-                        placeholder=PLACEHOLDER*CALL_LEVEL,
-                        module=func.__module__,
-                        func=func.__qualname__,
-                        out=res))
-                else:
-                    logger.debug("{placeholder}[{module}.{func}] >> {out}".format(
-                        placeholder=PLACEHOLDER*CALL_LEVEL,
-                        module=func.__module__,
-                        func=func.__qualname__,
-                        out=res))
-
-            else:
-                logger.error("{placeholder}[{module}.{func}] {error}".format(
-                    placeholder=PLACEHOLDER*CALL_LEVEL,
-                    module=func.__module__,
-                    func=func.__qualname__,
-                    error=res))
-            CALL_LEVEL -= 1
-            return suc, res
-
-    return wrapper
-
-
-def normalFuncLog(func):
-    """ More general auto logging decorator to function call
-
-    Function can return any form of data.
-    Handling all exception occured in this function, logging traceback and return None.
-
-    """
-    @functools.wraps(func)
-    def wrapper(*args, **kw):
-        global CALL_LEVEL, PLACEHOLDER
-        CALL_LEVEL += 1
-
-        if CALL_LEVEL == 0:
-            logger.info("{placeholder}[{module}.{func}] << {args} {kw}".format(
-                placeholder=PLACEHOLDER*CALL_LEVEL,
-                module=func.__module__,
-                func=func.__qualname__,
-                args=",".join(["{}".format(_arg) for _arg in args]),
-                kw=",".join(["{} = {}".format(k, v) for k, v in kw.items()])))
-        else:
-            logger.debug("{placeholder}[{module}.{func}] << {args} {kw}".format(
-                placeholder=PLACEHOLDER*CALL_LEVEL,
-                module=func.__module__,
-                func=func.__qualname__,
-                args=",".join(["{}".format(_arg) for _arg in args]),
-                kw=",".join(["{} = {}".format(k, v) for k, v in kw.items()])))
-
-        try:
-            out = func(*args, **kw)
-
-        except Exception as e:
-            logger.critical('[{module}.{func}] {trace}'.format(
-                module=func.__module__,
-                func=func.__qualname__,
-                trace=traceback.format_exc()))
-            CALL_LEVEL -= 1
-            return None
-
-        else:
-            if CALL_LEVEL == 0:
-                logger.info("{placeholder}[{module}.{func}] >> {out}".format(
-                    placeholder=PLACEHOLDER*CALL_LEVEL,
-                    module=func.__module__,
-                    func=func.__qualname__,
-                    out=out))
-            else:
-                logger.debug("{placeholder}[{module}.{func}] >> {out}".format(
-                    placeholder=PLACEHOLDER*CALL_LEVEL,
-                    module=func.__module__,
-                    func=func.__qualname__,
-                    out=out))
+            logger.debug("{placeholder}[{module}.{func}] >> {out}".format(
+                placeholder = PLACEHOLDER*CALL_LEVEL,
+                module      = func.__module__,
+                func        = func.__qualname__,
+                out         = out
+            ))
 
             CALL_LEVEL -= 1
             return out
-
-    return wrapper
-
-
-def APILog(func):
-    """ Auto logging decorator for restful api.
-
-    Logging resquest data if api method is POST. 
-    Handling all exception occured in this function, logging traceback.
-
-    """
-    @functools.wraps(func)
-    def wrapper(*args, **kw):
-        obj = args[0]
-        if func.__name__ == "post":
-            input_data = json.loads(obj.request.body)
-            logger.info("{api_name} <== {input}".format(
-                api_name=func.__qualname__,
-                input=input_data))
-
-        else:
-            logger.info("{api_name} <== ".format(
-                api_name=func.__qualname__))
-
-        try:
-            func(*args, **kw)
-
-        except Exception as e:
-            logger.critical("{api_name} {trace}".format(
-                api_name=func.__qualname__,
-                trace=traceback.format_exc()))
-
     return wrapper
